776cc96023f3537b66ef58914cf20121c7b7507c
ValECN <61169817+ValECN@users.noreply.github.com>
2021-04-17 15:44:57 +0200

    Respectralized
    During the ImHypAd project, we faced issues in the reconstruction of Hyperspectral Images (HSI). One step of the reconstruction using the Regularization Approach (RA) was to regularize the obtained HSI everywhere but on the contour. However, some HSI can have almost similar spectrums, which could occasionally create issues in the contour detection. Hence, changing the spectrums present in the HSI in order to maximize the difference between them can be used in order to facilitate the contour detection. However, the reconstruction will be done on the original HSI (here called IC for Input Cube) while the contour detection will be realized on the modified HSI (here called IC_modified).

--- respectralized.m
+++ respectralized.m
@@ -1,5 +1,5 @@
-load('A.mat')
-IC = permute(IC2, [2 3 1]);
+load('A.mat') % example input cube
+IC = permute(IC2, [2 3 1]); % input cube in format (R,C,W)
  
 IC_modified = respectralized_IC(IC,300,800);
 
@@ -14,7 +14,7 @@ plot(squeeze(IC_modified(2,10,:)))
 legend('IC spectra 2','IC modified spectra 2','IC spectra 1','IC modified spectra 1')
 title('Spectra comparison')
 
-function IC_modified = respectralized_IC(IC, lba_min, lba_max)
+function IC_modified = respectralized_IC(IC, lba_min, lba_max) % lba : lambda
 
 if isempty(lba_min)
     lba_min = 400; 
@@ -35,15 +35,15 @@ for x = 1:R
             liste_spectra = index;
         else
             if ismember(index,liste_spectra) ~= 1
-                liste_spectra(length(liste_spectra)+1) = index;
+                liste_spectra(length(liste_spectra)+1) = index;  % storing all the different spectrums
             end
         end
     end
 end
 
 N = length(liste_spectra);
-liste_spectra = sort(liste_spectra, 'ascend');
-pts = linspace(lba_min, lba_max, N);
+liste_spectra = sort(liste_spectra, 'ascend');                   % sorting in ascending order the spectrums
+pts = linspace(lba_min, lba_max, N);                             % creating equally separed points in the range(lba_min:lba_max)
 % interval = 0.5 * (pts(2) - pts(1));
 
 IC_modified = zeros(size(IC));
@@ -53,11 +53,11 @@ for x = 1:R
         [~, index] = max(s);
         for n = 1:N
             if index == liste_spectra(n)
-                gap = pts(n)-liste_spectra(n);
+                gap = pts(n)-liste_spectra(n);                   % quantify the difference between the real spectrum value and its attributed value
                 abs_gap = abs(pts(n)-liste_spectra(n));
                 for w = 1:W
                     if gap > 0
-                        if mod(w+abs_gap,W) == 0
+                        if mod(w+abs_gap,W) == 0                 % trick to skip the 0 value obtained for mod(x,x)
                             modulo = 1;
                         else 
                             modulo = mod(w+abs_gap,W);
