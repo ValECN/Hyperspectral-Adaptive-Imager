76234914d4a156f8832b985e500d7579bb3b5d97
Valentin <val.noel2369@gmail.com>
2021-04-18 19:02:38 +0200

    Adding gradient x/y as outputs

--- contour_detection.m
+++ contour_detection.m
@@ -1,6 +1,6 @@
 %% Computing the contours of the image using its isolines
 
-I = contour_from_isolines(panchro);
+[I, Gr_x, Gr_y] = contour_from_isolines(panchro);
 
 % --- Plot
 figure 
@@ -9,18 +9,22 @@ title('Contours of the input image')
 xlabel('X\_cam')
 ylabel('Y\_cam')
 
-function Gr = contour_from_isolines(I)
+function [Gr, Gr_x, Gr_y] = contour_from_isolines(I)
 
 Gr = zeros(size(I));
 [R, ~] = size(I);
-c = contour(I);
-c(c>R) = -1;                                % To prevent incoherent values given by contour
+isolines = contour(I);
+isolines(isolines>R) = -1; % To prevent incoherent values given by contour()
 
-for k = 1:length(c)
-    if c(1,k) >= 0 && c(2,k) >=0
-        Gr(round(c(2,k)),round(c(1,k))) = 1;
+for k = 1:length(isolines)
+    if isolines(1,k) >= 0 && isolines(2,k) >=0
+        Gr(round(isolines(2,k)),round(isolines(1,k))) = 1;
     end
 end
+
+Gr_x = diff(I,1,2);
+Gr_y = diff(I,1,1);
+
 end
 
     
